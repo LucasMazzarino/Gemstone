8c2b97b2705397d5c9580c68a36c092d2277b798
Lucas Mazzarino <Mazzanga1212@gmail.com>
2024-05-15 21:44:25 -0300

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,10 @@ const nextConfig = {
 				protocol: "https",
 				hostname: "gemstoneuruguay-production.up.railway.app",
 			},
+			{
+				protocol: "https",
+				hostname: "gemstonuruguay.com",
+			},
 		],
   },
   logging: {
